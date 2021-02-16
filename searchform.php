756dbe3778310b527c9152eb1314d1ddfa1dc293
vdenis96 <vornicu.denis@gmail.com>
2021-02-16 13:08:54 +0200

    "structure update"

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">    
-    <div>
-        <label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'mr_tailor' ); ?></label>
-        <input type="search" class="search-field" id="s" placeholder="<?php _e( 'Zoeken...', 'mr_tailor' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
-        <input type="submit" class="search-submit" value="<?php _e( 'Zoeken', 'mr_tailor' ); ?>">
-    </div>
-</form>
